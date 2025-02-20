d6cc763dace11158010e4441a028ea25720bafe0
WillianRRP <willianrenato754@gmail.com>
2025-02-20 19:41:56 -0300

    creating login tests

---
+++ automatizado/src/test/java/automatizado/Page/LoginPO.java
@@ -0,0 +1,11 @@
+package automatizado.Page;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPO extends BasePO{
+
+    public LoginPO(WebDriver driver) {
+        super(driver);
+    }
+    
+}
